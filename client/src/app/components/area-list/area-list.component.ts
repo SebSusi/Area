289585e91088c20745459bf8719f69716988961f
Yannick Suc <suc.yannick@gmail.com>
2019-02-12 19:17:34 +0100

    [Front] Responsive

--- client/src/app/components/area-list/area-list.component.ts
+++ client/src/app/components/area-list/area-list.component.ts
@@ -1,8 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {Area, AreaAdapter} from '../../objects/area';
 import {HttpClient} from '@angular/common/http';
-import {from, Observable, of} from 'rxjs';
-import {map} from 'rxjs/operators';
 import {AreaService} from '../../services/area.service';
 
 @Component({
@@ -17,7 +15,6 @@ export class AreaListComponent implements OnInit {
     private adapter = new AreaAdapter();
 
     constructor(private http: HttpClient, private areaService: AreaService) {
-        const names = ['Envois des mails', 'GregStalker', 'Activity Alert', 'Dis bonjour sur facebook', 'Mail Translator'];
         areaService.getAreas().subscribe(value => {this.areas = value; });
         areaService.getArea('id').subscribe(value => {this.areas.push(value); });
     }
