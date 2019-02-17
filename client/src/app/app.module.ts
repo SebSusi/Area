1469f5c29060bc80b35418ad4cd03f7595ee3bf4
Yannick Suc <suc.yannick@gmail.com>
2019-02-17 04:23:14 +0100

    [Front] Add Forms

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,43 +1,60 @@
+import {AccountManagerComponent} from './components/actions-managers/account-manager/account-manager.component';
+import {ActionComponent} from './components/actions-managers/action/action.component';
 import {AngularSvgIconModule} from 'angular-svg-icon';
+import {AppComponent} from './app.component';
+import {AreaItemComponent} from './components/area-item/area-item.component';
+import {AreaListComponent} from './components/area-list/area-list.component';
+import {AuthServiceConfig, SocialLoginModule} from 'angularx-social-login';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {BrowserModule} from '@angular/platform-browser';
+import {ButtonComponent} from './components/actions-managers/options-manager/form-components/button/button.component';
+import {ButtonsModule} from 'angular-bootstrap-md';
+import {CheckboxComponent} from './components/actions-managers/options-manager/form-components/checkbox/checkbox.component';
+import {DateComponent} from './components/actions-managers/options-manager/form-components/date/date.component';
+import {DynamicFieldDirective} from './components/actions-managers/options-manager/form-components/dynamic-field/dynamic-field.directive';
+import {DynamicFormComponent} from './components/actions-managers/options-manager/form-components/dynamic-form/dynamic-form.component';
+import {EditorComponent} from './components/pages/editor/editor.component';
+import {Error404Component} from './components/error404/error404.component';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {getAuthServiceConfigs} from './objects/socialLoginConfig';
+import {HomeComponent} from './components/pages/home/home.component';
 import {HttpClientModule} from '@angular/common/http';
-import {RouterModule, Routes} from '@angular/router';
+import {InputComponent} from './components/actions-managers/options-manager/form-components/input/input.component';
+import {LoginComponent} from './components/pages/login/login.component';
+import {NavbarComponent} from './components/navbar/navbar.component';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 import {NgModule} from '@angular/core';
-import {AuthServiceConfig, SocialLoginModule} from 'angularx-social-login';
+import {OptionsManagerComponent} from './components/actions-managers/options-manager/options-manager.component';
+import {RadiobuttonComponent} from './components/actions-managers/options-manager/form-components/radiobutton/radiobutton.component';
+import {RouterModule, Routes} from '@angular/router';
+import {SelectComponent} from './components/actions-managers/options-manager/form-components/select/select.component';
+import {ServiceManagerComponent} from './components/actions-managers/app-type-manager/service-manager.component';
+import {TriggerManagerComponent} from './components/actions-managers/trigger-manager/trigger-manager.component';
+import {UserAppComponent} from './components/pages/user-app/user-app.component';
+import {UserComponent} from './components/pages/user/user.component';
 
 import {
     MatAutocompleteModule,
-    MatButtonModule, MatCardModule,
+    MatButtonModule,
+    MatCardModule,
     MatCheckboxModule,
     MatChipsModule,
+    MatDatepickerModule,
     MatDialogModule,
-    MatDividerModule, MatFormFieldModule,
-    MatIconModule, MatInputModule, MatListModule, MatProgressBarModule, MatProgressSpinnerModule,
-    MatSlideToggle, MatSlideToggleModule, MatTabsModule, MatTreeModule, MatRadioModule
+    MatDividerModule,
+    MatFormFieldModule,
+    MatIconModule,
+    MatInputModule,
+    MatListModule,
+    MatProgressBarModule,
+    MatProgressSpinnerModule,
+    MatRadioModule,
+    MatSelectModule,
+    MatSlideToggleModule,
+    MatTabsModule,
+    MatTreeModule
 } from '@angular/material';
 
-
-import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
-import {ButtonsModule} from 'angular-bootstrap-md';
-
-import {AppComponent} from './app.component';
-import {LoginComponent} from './components/pages/login/login.component';
-import {HomeComponent} from './components/pages/home/home.component';
-import {UserComponent} from './components/pages/user/user.component';
-import {NavbarComponent} from './components/navbar/navbar.component';
-import {Error404Component} from './components/error404/error404.component';
-import {getAuthServiceConfigs} from './objects/socialLoginConfig';
-import {AreaListComponent} from './components/area-list/area-list.component';
-import {AreaItemComponent} from './components/area-item/area-item.component';
-import { EditorComponent } from './components/pages/editor/editor.component';
-import { UserAppComponent } from './components/pages/user-app/user-app.component';
-import { ServiceManagerComponent } from './components/actions-managers/app-type-manager/service-manager.component';
-import { TriggerManagerComponent } from './components/actions-managers/trigger-manager/trigger-manager.component';
-import { OptionsManagerComponent } from './components/actions-managers/options-manager/options-manager.component';
-import { ActionComponent } from './components/actions-managers/action/action.component';
-import { AccountManagerComponent } from './components/actions-managers/account-manager/account-manager.component';
 const appRoutes: Routes = [
     {
         path: '',
@@ -82,54 +99,71 @@ const appRoutes: Routes = [
 
 @NgModule({
     declarations: [
+        AccountManagerComponent,
+        ActionComponent,
         AppComponent,
+        AreaItemComponent,
+        AreaListComponent,
+        ButtonComponent,
+        CheckboxComponent,
+        DateComponent,
+        DynamicFieldDirective,
+        DynamicFormComponent
+        EditorComponent,
+        Error404Component,
+        HomeComponent,
+        InputComponent,
         LoginComponent,
         NavbarComponent,
-        HomeComponent,
-        UserComponent,
-        Error404Component,
-        AreaListComponent,
-        AreaItemComponent,
-        EditorComponent,
-        UserAppComponent,
+        OptionsManagerComponent,
+        RadiobuttonComponent,
+        SelectComponent,
         ServiceManagerComponent,
         TriggerManagerComponent,
-        OptionsManagerComponent,
-        ActionComponent,
-        AccountManagerComponent
+        UserAppComponent,
+        UserComponent,
     ],
-    entryComponents: [],
+    entryComponents: [
+        InputComponent,
+        ButtonComponent,
+        SelectComponent,
+        DateComponent,
+        RadiobuttonComponent,
+        CheckboxComponent
+    ],
     imports: [
         RouterModule.forRoot(
             appRoutes,
             {enableTracing: false}
         ),
-        HttpClientModule,
-        BrowserModule,
-        ReactiveFormsModule,
-        FormsModule,
         AngularSvgIconModule,
-        NgbModule,
         BrowserAnimationsModule,
-        MatButtonModule,
-        MatCheckboxModule,
-        MatFormFieldModule,
-        MatInputModule,
-        MatDialogModule,
-        MatCardModule,
-        MatIconModule,
-        MatTreeModule,
-        MatTabsModule,
-        MatListModule,
-        MatDividerModule,
+        BrowserModule,
+        ButtonsModule,
+        FormsModule,
+        HttpClientModule,
         MatAutocompleteModule,
+        MatButtonModule,
+        MatCardModule,
+        MatCheckboxModule,
         MatChipsModule,
+        MatDatepickerModule,
+        MatDialogModule,
+        MatDividerModule,
+        MatFormFieldModule,
+        MatIconModule,
+        MatInputModule,
+        MatListModule,
         MatProgressBarModule,
         MatProgressSpinnerModule,
-        MatSlideToggleModule,
         MatRadioModule,
-        ButtonsModule,
-        SocialLoginModule
+        MatSelectModule,
+        MatSlideToggleModule,
+        MatTabsModule,
+        MatTreeModule,
+        NgbModule,
+        ReactiveFormsModule,
+        SocialLoginModule,
     ],
     providers: [{
         provide: AuthServiceConfig,
