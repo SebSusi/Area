328d8d82e33df54ffb118df00efb30f49d02539d
julian ladjani <julian.ladjani@epitech.eu>
2019-03-08 14:23:22 +0100

    [Server] Area add action/reaction

--- server/src/api/controllers/area/setters.js
+++ server/src/api/controllers/area/setters.js
@@ -1,13 +1,56 @@
 const _ = require('lodash');
-const area = require('../../models/area/Area');
+const Area = require('../../models/area/Area');
 
-exports.createArea = function (req){
-    let newArea = new area();
+async function save(area) {
+    return await area.save(function (err, object) {
+        if (err)
+            return false;
+        return object;
+    });
+}
+
+exports.saveAction = async function (area, id, serviceName, name) {
+    area.action = {
+        id: id,
+        name: name,
+        serviceName: serviceName,
+    };
+    return await save(area);
+};
+
+exports.addReaction = async function (area, id, serviceName, name) {
+    area.reactions.push({
+        id: id,
+        name: name,
+        serviceName: serviceName,
+    });
+    return await save(area);
+};
+
+exports.deleteReaction = async function (area, reaction) {
+    _.remove(area.reactions, function (savedReaction) {
+        return savedReaction.id === reaction.id;
+    });
+    return await save(area);
+};
+
+exports.deleteReactions = async function (area) {
+    area.reactions = [];
+    return await save(area);
+};
+
+exports.deleteAction = async function (area) {
+    area.action = null;
+    return await save(area);
+};
+
+exports.createArea = async function (req) {
+    let newArea = new Area();
     newArea.params = params;
     newArea.ownerId = req.user._id;
     newArea.name = req.body.name;
     newArea.activated = req.body.activated;
     newArea.timer = req.body.timer;
-    newArea.save();
+    newArea = await save(newArea);
     return {id: newArea._id, success: true};
 };
--- server/src/api/controllers/area/widget/setters.js
+++ server/src/api/controllers/area/widget/setters.js
@@ -1,4 +1,15 @@
 const _ = require('lodash');
+const widgetGetter = require('./getters');
+const schemaGetter = require('../../models/widget/schemaGetter');
+const areaSetter = require('../setters');
+
+async function save(widget) {
+    return await widget.save(function (err, object) {
+        if (err)
+            return false;
+        return object;
+    });
+}
 
 exports.setParamIfExist = function (paramsObj, paramKey, valueObj, valueKey, params, update) {
     if (paramKey !== undefined && paramKey !== '') {
@@ -21,9 +32,35 @@ exports.setWidgetParams = function (req, model, params, update) {
     let paramsObj = {};
     let modelParams = schemaGetter.getModelSchemaParams(model);
     modelParams.forEach(function (modelParam) {
-        exports.setParamIfExist(paramsObj, modelParam, req, 'body.' + modelParam, params, update);
+        exports.setParamIfExist(paramsObj, modelParam, req, 'body.fields' + modelParam, params, update);
     });
     return paramsObj;
 };
 
-exports.createArea()
+exports.addAction = async function (req, area) {
+    let action = widgetGetter.getActionByServiceNameAndActionName(req.body.serviceName, req.body.name);
+    let model = action.model;
+    let params = exports.setWidgetParams(req, model, undefined, false);
+    if (params === false)
+        return {id: false, success: false};
+    let newAction = new model();
+    newAction.params = params;
+    newAction.user.id = req.user._id;
+    newAction = await save(newAction);
+    await areaSetter.saveAction(area, newAction._id, req.body.serviceName, req.body.name);
+    return {id: newAction._id, success: true};
+};
+
+exports.addReaction = async function (req, area) {
+    let reaction = widgetGetter.getReactionByServiceNameAndReactionName(req.body.serviceName, req.body.name);
+    let model = reaction.model;
+    let params = exports.setWidgetParams(req, model, undefined, false);
+    if (params === false)
+        return {id: false, success: false};
+    let newReaction = new model();
+    newReaction.params = params;
+    newReaction.user.id = req.user._id;
+    newReaction = await save(newReaction);
+    await areaSetter.addReaction(area, newReaction._id, req.body.serviceName, req.body.name);
+    return {id: newReaction._id, success: true};
+};
