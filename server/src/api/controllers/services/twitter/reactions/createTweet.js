d96bc30df54e45914f73cf2ca1bffca6c10fd5b8
Yannick Suc <suc.yannick@gmail.com>
2019-03-12 03:51:15 +0100

    [Server] Create tweet support mediaq

--- server/src/api/controllers/services/twitter/reactions/createTweet.js
+++ server/src/api/controllers/services/twitter/reactions/createTweet.js
@@ -1,63 +1,41 @@
 const Twitter = require('twitter');
 const http = require('http');
 
-
-async function getImage(url, callback) {
-    console.log('[' + url + ']');
-    if (!url.length)
-        callback(null, data);
-    http.get(url, res => {
-        const bufs = [];
-        res.on('data', function (chunk) {
-            bufs.push(chunk)
-        });
-        res.on('end', function () {
-            const data = Buffer.concat(bufs);
-            callback(null, data);
-        });
-    }).on('error', callback).catch((err) => {
-        return Observable.throw(err)
+async function uploadImage(data, client) {
+    return new Promise(function (resolve, reject) {
+        client.post(
+            'media/upload',
+            {media: data},
+            function (error, media, response) {
+                if (error)
+                    resolve(null);
+                else
+                    resolve(media.media_id_string);
+            }
+        );
     });
 }
 
-async function uploadFile(mediaUrls, client, nextImg, sender) {
-    if (mediaUrls.length)
-        getImage(mediaUrl[0], function (err, data) {
-            nextImg({media_id: 1105153134995955700});
-            /*              client.post(
-                            'media/upload',
-                            {media: data},
-                            function(error, media, response) {
-                                console.log(media);
-                                callback(media);
-                            }
-                        );*/
-        });
-    else
-        sender();
-    /*    await base64Img.requestBase64(mediaUrl, function(err, res, body) {
-    //        console.log(body);
-            client.post('media/upload',
-                { media_data: body.toString() }, function (err, data, response) {
-                console.log(data);
+async function getImage(url, client) {
+    return new Promise(function (resolve, reject) {
+        var req = http.request(url, function (res) {
+            if (res.statusCode < 200 || res.statusCode >= 300) {
+                resolve(null);
+            }
+            const bufs = [];
+            res.on('data', function (chunk) {
+                bufs.push(chunk);
             });
-        });*/
-}
-
-async function customAlert(msg, client, ids, callback) {
-    console.log();
-    getImage(msg, function (err, data) {
-        data = {media_id: 1105153134995955700};
-        ids += data.media_id + ',';
-        callback();
-        /*              client.post(
-                        'media/upload',
-                        {media: data},
-                        function(error, media, response) {
-                            console.log(media);
-                            callback(media);
-                        }
-                    );*/
+            res.on('end', function () {
+                const data = Buffer.concat(bufs);
+                const media = uploadImage(data, client);
+                resolve(media);
+            });
+        });
+        req.on('error', function (err) {
+            resolve(null);
+        });
+        req.end();
     });
 }
 
@@ -68,22 +46,21 @@ exports.doReaction = async function (reaction, reactionInfo, parsedParams, accou
         access_token_key: account.accessToken,
         access_token_secret: account.secret
     });
-    /*let ids='';
     const medias = [parsedParams.media1, parsedParams.media2, parsedParams.media3, parsedParams.media4];
-    var x = 0;
-    var loopThrewMedia = function () {
-        console.log(x);
-        customAlert(medias[x], client, ids, function () {
-            x++;
-            console.log(medias.length);
-            if (x < medias.length) {
-                loopThrewMedia();
-                console.log(x);
-            }
-            console.log(x);
-        });
-    };
-
-    loopThrewMedia();*/
-    client.post('statuses/update', {status: parsedParams.message},  function(error, tweet, response) {});
+    const ids = [];
+    for (const media of medias) {
+        try {
+            ids.push(await getImage(media, client));
+        } catch (e) {
+        }
+    }
+    client.post('statuses/update', {
+        status: parsedParams.message,
+        media_ids: ids.join(',')
+    }, function (error, tweet, response) {
+        if (error)
+            console.log(error);
+        else
+            console.log('New Tweet !');
+    });
 };
